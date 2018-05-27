1740a93c3bf3266b1ac626f52a080a8e677eeb34
Jon Brennecke <jpbrennecke@gmail.com>
2018-05-27 15:07:16 -0700

    Optimized nginx configuration

--- src/App.js
+++ src/App.js
@@ -35,9 +35,8 @@ export default class App extends Component<DefaultProps, Props, State> {
             <meta charSet="utf-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no" />
-            <link rel="stylesheet" href="animate.min.css"/>
             <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-            <link rel="canonical" href="http://mysite.com/example" />
+            <link rel="canonical" href="http://mathhacks.co" />
             <title>Math Hacks</title>
             <style>{`
               html, body, main {
