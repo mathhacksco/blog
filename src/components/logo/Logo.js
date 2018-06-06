037907e56c9bb07aca9b14b12f38b0032682c320
Jon Brennecke <jpbrennecke@gmail.com>
2018-06-06 15:29:58 -0700

    added logo svg and navigation styles

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -4,14 +4,17 @@ import React from 'react';
 // $FlowFixMe
 import './Logo.styles.scss';
 // $FlowFixMe
-import LogoSvg from './logo.svg';
+import LogoTextSvg from './logoText.svg';
+// $FlowFixMe
+import LogoIconSvg from './logoIcon.svg';
 
 type Props = {};
 
 export default function Logo(props: Props) {
   return (
     <div className="logo-container">
-      <div className="logo" dangerouslySetInnerHTML={{ __html: LogoSvg }}/>
+      <div className="logo logo-icon" dangerouslySetInnerHTML={{ __html: LogoIconSvg }}/>
+      <div className="logo logo-text" dangerouslySetInnerHTML={{ __html: LogoTextSvg }}/>
     </div>
   );
 }
