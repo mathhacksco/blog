fe000c3b3d413f0511affd23d52f352e8b791f62
Jon Brennecke <jpbrennecke@gmail.com>
2018-06-09 11:44:58 -0700

    deployment hotfix: fixing variables that are undefined in Travis build

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -3,8 +3,8 @@ if [ -z "$ANSIBLE_VAULT_PASSWORD" ]; then
   exit 1
 fi
 
-export GIT_SHA=$(git rev-parse HEAD)
-export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export GIT_SHA=$(if [ -z "$TRAVIS_COMMIT" ]; then git rev-parse HEAD; else echo $TRAVIS_COMMIT; fi)
+export GIT_BRANCH=$(if [ -z "$TRAVIS_BRANCH" ]; then git rev-parse --abbrev-ref HEAD; else echo $TRAVIS_BRANCH; fi)
 
 ansible-playbook \
     -M command \
