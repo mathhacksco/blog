eaeb0c0c3d292360de22a79738f158225e985739
Jon Brennecke <jpbrennecke@gmail.com>
2018-06-10 00:18:47 -0700

    post content styles

--- src/components/post-content/PostContent.js
+++ src/components/post-content/PostContent.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 // $FlowFixMe
 import './PostContent.scss';
@@ -14,16 +13,7 @@ type Props = {
 export default function PostContent({ post }: Props) {
   return (
     <div className="post-content-container">
-      {/* {this.renderTags()} */}
-      <p className="post-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }}/>
+      <article className="post-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }}/>
     </div>
   );
 }
-
-// function renderTags(tags: string[]) {
-//   return this.props.tags.map(tag => (
-//     <Link to={`/tags/${tag.id}`}>
-//       <div>{tag.name}</div>
-//     </Link>
-//   ));
-// }
