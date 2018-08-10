fa600b60c3a16c99f83075e07424cae1508ac3f9
Jon Brennecke <jpbrennecke@gmail.com>
2018-08-09 20:57:16 -0700

    improved call to action button UI

--- src/components/call-to-action-buttons/CallToActionButtons.js
+++ src/components/call-to-action-buttons/CallToActionButtons.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import { SOCIAL_LINKS } from '../../constants';
 import ContentMaxWidth from '../layout/content-max-width/ContentMaxWidth';
 import HorizontallyCentered from '../layout/horizontally-centered/HorizontallyCentered';
+import YoutubeBackgroundImage from 'assets/images/call-to-action/YoutubeBackground.png';
+import InstagramBackgroundImage from 'assets/images/call-to-action/InstagramBackground.png';
 
 // $FlowFixMe
 import './CallToActionButtons.scss';
@@ -20,25 +23,34 @@ export default function CallToActionButtons({ className }: Props) {
     >
       <ContentMaxWidth className="call-to-action-inner">
         <div className="call-to-action-button">
-          <div className="background" />
-          <div className="content">
-            <span className="call-to-action-text-secondary">MathHacks on Youtube</span>
-            <h2 className="call-to-action-text-primary">Watch Now</h2>
+          <div className="background">
+            <img src={YoutubeBackgroundImage} />
           </div>
-        </div>
-        <div className="call-to-action-button">
-          {/* TODO: follow in Instagram */}
-          <div className="background" />
           <div className="content">
-            <span className="call-to-action-text-secondary">{'Don\'t be left out'}</span>
-            <h2 className="call-to-action-text-primary">Get the Study Guide</h2>
+            <a href={SOCIAL_LINKS.YOUTUBE}>
+              <span className="call-to-action-text-secondary">MathHacks on Youtube</span>
+              <h2 className="call-to-action-text-primary"><span>Watch Now</span></h2>
+            </a>
           </div>
         </div>
         <div className="call-to-action-button">
           <div className="background" />
           <div className="content">
-            <span className="call-to-action-text-secondary">MathHacks on Youtube</span>
-            <h2 className="call-to-action-text-primary">Subscribe Now</h2>
+            <a href={SOCIAL_LINKS.INSTAGRAM}>
+              <span className="call-to-action-text-secondary">{'Don\'t be left out'}</span>
+              <h2 className="call-to-action-text-primary"><span>Get the Study Guide</span></h2>
+            </a>
+          </div>
+        </div>
+        <div className="call-to-action-button">
+          <div className="background">
+            <img src={InstagramBackgroundImage} />
+          </div>
+          <div className="content">
+            <a href={SOCIAL_LINKS.INSTAGRAM}>
+              <span className="call-to-action-text-secondary">MathHacks on Instagram</span>
+              <h2 className="call-to-action-text-primary"><span>Follow along</span></h2>
+            </a>
           </div>
         </div>
       </ContentMaxWidth>
